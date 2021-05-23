be796cc16bfcf19f4e5baf24c8f337ab07be7139
Mehmet Kaya <81460760+gafakaya@users.noreply.github.com>
2021-05-23 09:38:56 +0300

    23/05/21

--- hrms2/src/main/java/kodlama/io/hrms2/entities/concretes/JobTitle.java
+++ hrms2/src/main/java/kodlama/io/hrms2/entities/concretes/JobTitle.java
@@ -3,6 +3,7 @@ package kodlama.io.hrms2.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,7 +15,7 @@ import lombok.Data;
 public class JobTitle {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 
