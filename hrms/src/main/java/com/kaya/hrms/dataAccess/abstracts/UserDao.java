7720e772cdffd144675a1333dee65b0017f95d56
Mehmet Kaya <81460760+gafakaya@users.noreply.github.com>
2021-05-24 17:42:00 +0300

    24/05/21 UserDao

--- hrms/src/main/java/com/kaya/hrms/dataAccess/abstracts/UserDao.java
+++ hrms/src/main/java/com/kaya/hrms/dataAccess/abstracts/UserDao.java
@@ -3,9 +3,11 @@ package com.kaya.hrms.dataAccess.abstracts;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.kaya.hrms.entities.concretes.User;
 
+@Repository
 public interface UserDao<T extends User, ID> extends JpaRepository<T, ID> {
 
 	Optional<T> findByEmail(String email);
